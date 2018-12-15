7afd682c8814b96d81bce0fad06b7a2167fa2559
dmb-2222 <dmb-2222@yandex.ru>
2018-12-15 21:03:15 +0200

    home work v.1

--- js/module-n/module-2/addhomework/js/index.js
+++ js/module-n/module-2/addhomework/js/index.js
@@ -6,17 +6,18 @@ let userEnter;
 do {
   userEnter = prompt("Введите ваш пароль");
   for (let i of passwords) {
+    attempts = attempts - 1;
     if (passwords.includes(userEnter)) {
       alert("Добро пожаловать");
       attempts = attempts - 3;
       break;
     } else if (attempts > 0) {
-      attempts = attempts - 1;
       console.log(attempts);
       alert(`Неверный пароль, у вас осталось ${attempts} попыток`);
       break;
     } else if (attempts === 0) {
       alert("У вас закончились попытки, аккаунт заблокирован!");
+      break;
     }
   }
 } while (attempts > 0);
