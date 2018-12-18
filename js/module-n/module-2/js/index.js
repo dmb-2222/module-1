3ba1bc493d7908ccc5fdcbb918b92e8c831ceff5
dmb-2222 <dmb-2222@yandex.ru>
2018-12-18 19:20:03 +0200

    home work v. 18.12.18

--- js/module-n/module-2/js/index.js
+++ js/module-n/module-2/js/index.js
@@ -1,24 +1,25 @@
-'use strict';
+"use strict";
 let userInput;
 let inp;
 const numbers = [];
 let total = 0;
 do {
-    userInput = prompt('Введите любое число');
-    let asNumber = Number(userInput);
-    if (Number.isNaN(asNumber) === true){
-        alert('Было введено не число, попробуйте еще раз');
-    }else (numbers.push(asNumber));
+  userInput = prompt("Введите любое число");
+  let asNumber = Number(userInput);
+  if (Number.isNaN(asNumber) === true) {
+    alert("Было введено не число, попробуйте еще раз");
+  } else {
+    numbers.push(asNumber);
     console.log(numbers);
+  }
 } while (userInput !== null);
 
 // Первый способ------------------------------
-for (let i = 0; i<numbers.length; i++){
-total += numbers[i];
+for (let i = 0; i < numbers.length; i++) {
+  total += numbers[i];
 }
 alert(`Сумма чисел равна ${total}`);
 console.log(total);
-
 
 //  Второй способ ---------------------
 // for(let i of numbers){
@@ -26,7 +27,6 @@ console.log(total);
 // }
 // alert(`Сумма чисел равна ${total}`);
 // console.log(total);
-
 
 // Третий способ----------------------
 // numbers.forEach(d=>{
