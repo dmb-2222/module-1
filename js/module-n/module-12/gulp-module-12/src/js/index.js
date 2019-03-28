f496b38b81682044bd9c0d320d48bd89e6412465
dmb-2222 <dmb-2222@yandex.ru>
2019-03-28 21:46:29 +0200

    m12 v2

--- js/module-n/module-12/gulp-module-12/src/js/index.js
+++ js/module-n/module-12/gulp-module-12/src/js/index.js
@@ -7,21 +7,40 @@ const tempUrl = document.querySelector("#temp-url").innerHTML.trim();
 const mark = Handlebars.compile(tempUrl);
 let arr = [];
 let resultInput;
+let i = 0;
+
 function userInput(e) {
   e.preventDefault();
+  i++;
   resultInput = input.value;
-  arr.push({ url: `${input.value}` });
+  console.log(input.value);
+  arr.push({ id: i, url: input.value });
   let result = arr.reduce((acc, el) => acc + mark(el), "");
   allUrl.innerHTML = result;
+
   form.reset();
 }
 form.addEventListener("submit", userInput);
 
-function userDelete(e) {
+// function userDelete(e) {
+//   e.preventDefault();
+//   // const divContainer = document.querySelector(".container");
+//   // const del = document.getElementById(targetId);
+
+//   const btn = document.querySelector("button");
+//   btn.dataset.targetId;
+//   console.log(`del.dataset.targetId= ${btn.dataset.targetId}`);
+
+//   const idToRemove = 1;
+//   const filteredUrl = arr.filter(item => item.id !== idToRemove);
+//   let result = filteredUrl.reduce((acc, el) => acc + mark(el), "");
+//   allUrl.innerHTML = result;
+//   console.log(filteredUrl);
+// }
+// allUrl.addEventListener("click", userDelete);
+
+allUrl.addEventListener('click', function(e){
   e.preventDefault();
-  console.log(`input.value = ${resultInput}`);
-  const del = document.getElementById(resultInput);
-  console.log(`del.dataset.targetId= ${del.dataset.targetId}`);
-  del.remove();
-}
-allUrl.addEventListener("click", userDelete);
+  const {targetId} = this.dataset;
+  document.querySelector(`[data-target-id="${targetId}"]`).remove();
+})
