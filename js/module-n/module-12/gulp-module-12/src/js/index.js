6ab4728a21481d2c3011839c276170f681b513b2
dmb-2222 <dmb-2222@yandex.ru>
2019-04-05 17:55:21 +0300

    v.05.04.19

--- js/module-n/module-12/gulp-module-12/src/js/index.js
+++ js/module-n/module-12/gulp-module-12/src/js/index.js
@@ -1,46 +1,67 @@
 "use strict";
 const input = document.querySelector("#urls");
 const form = document.querySelector("#post");
-
 const allUrl = document.querySelector(".all-url");
 const tempUrl = document.querySelector("#temp-url").innerHTML.trim();
 const mark = Handlebars.compile(tempUrl);
 let arr = [];
-let resultInput;
 let i = 0;
 
+// LocalStorege
+const settingsFromLS = JSON.parse(localStorage.getItem("arr"));
+if (settingsFromLS !== null) {
+  const lastId = settingsFromLS.reduce((acc, el) => el.id, 0);
+  i = lastId;
+  arr = settingsFromLS;
+  let result = arr.reduce((acc, el) => acc + mark(el), "");
+  allUrl.innerHTML = result;
+}
+console.log(arr);
 function userInput(e) {
   e.preventDefault();
   i++;
-  resultInput = input.value;
-  console.log(input.value);
-  arr.push({ id: i, url: input.value });
-  let result = arr.reduce((acc, el) => acc + mark(el), "");
-  allUrl.innerHTML = result;
+  const isValidInput = input.value.length !== 0;
+  const isValidUrls = arr.find(urles => urles.url === input.value);
+  console.log(arr);
+  if (isValidInput && !isValidUrls) {
+    if (settingsFromLS === null) {
+      arr.push({ id: i, url: input.value });
+      let result = arr.reduce((acc, el) => acc + mark(el), "");
+      localStorage.setItem("arr", JSON.stringify(arr));
+      // let result = settingsFromLS.reduce((acc, el) => acc + mark(el), "");
+      allUrl.innerHTML = result;
+    } else {
+      arr.push({ id: i, url: input.value });
+      let result = settingsFromLS.reduce((acc, el) => acc + mark(el), "");
+      localStorage.setItem("arr", JSON.stringify(arr));
+      allUrl.innerHTML = result;
+    }
 
+    // Удаление урла через кнопку удалить
+    const btns = Array.from(document.querySelectorAll(".remove"));
+
+    btns.forEach(btn => {
+      btn.addEventListener("click", function(e) {
+        if (settingsFromLS === null) {
+          arr = arr.filter(el => el.id !== +this.dataset.targetId);
+          document
+            .querySelector(`[data-id="${this.dataset.targetId}"]`)
+            .remove();
+          localStorage.setItem("arr", JSON.stringify(arr));
+        } else {
+          arr = settingsFromLS;
+          arr = arr.filter(el => el.id !== +this.dataset.targetId);
+          document
+            .querySelector(`[data-id="${this.dataset.targetId}"]`)
+            .remove();
+          // localStorage.setItem("arr", JSON.stringify(arr));
+        }
+      });
+    });
+  } else alert("Вы ввели пустое значение или такой адрес уже есть");
+  localStorage.setItem("arr", JSON.stringify(arr));
   form.reset();
 }
 form.addEventListener("submit", userInput);
 
-// function userDelete(e) {
-//   e.preventDefault();
-//   // const divContainer = document.querySelector(".container");
-//   // const del = document.getElementById(targetId);
-
-//   const btn = document.querySelector("button");
-//   btn.dataset.targetId;
-//   console.log(`del.dataset.targetId= ${btn.dataset.targetId}`);
-
-//   const idToRemove = 1;
-//   const filteredUrl = arr.filter(item => item.id !== idToRemove);
-//   let result = filteredUrl.reduce((acc, el) => acc + mark(el), "");
-//   allUrl.innerHTML = result;
-//   console.log(filteredUrl);
-// }
-// allUrl.addEventListener("click", userDelete);
-
-allUrl.addEventListener('click', function(e){
-  e.preventDefault();
-  const {targetId} = this.dataset;
-  document.querySelector(`[data-target-id="${targetId}"]`).remove();
-});
+// localStorage.clear();
