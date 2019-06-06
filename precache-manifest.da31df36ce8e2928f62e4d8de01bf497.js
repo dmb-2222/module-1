01132b368511db6019fb14f7f00884bb21817912
dmb-2222 <dmb-2222@yandex.ru>
2019-06-06 20:10:32 +0300

    Updates

---
+++ precache-manifest.da31df36ce8e2928f62e4d8de01bf497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669e8b2f47d645de57f2978dfcbb3162",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "458237979d803d1cee65",
+    "url": "/goit-fe-course/static/css/main.30264d9b.chunk.css"
+  },
+  {
+    "revision": "19bb0470a1309715ed77",
+    "url": "/goit-fe-course/static/js/2.35284788.chunk.js"
+  },
+  {
+    "revision": "458237979d803d1cee65",
+    "url": "/goit-fe-course/static/js/main.6c98764d.chunk.js"
+  },
+  {
+    "revision": "6fc20f7e957d07e7940e",
+    "url": "/goit-fe-course/static/js/runtime~main.e6ce5aa7.js"
+  }
+]);
