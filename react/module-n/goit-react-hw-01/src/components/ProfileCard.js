3fc7ef5173e1d92d2c9a47392e6a9910e715a78a
dmb-2222 <dmb-2222@yandex.ru>
2019-06-07 18:10:07 +0300

    v3

--- react/module-n/goit-react-hw-01/src/components/ProfileCard.js
+++ react/module-n/goit-react-hw-01/src/components/ProfileCard.js
@@ -2,30 +2,39 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ProfileCard.module.css";
 
-const ProfileCard = ({ props }) => (
-  <div className={styles.profile}>
-    <div className={styles.description}>
-      <img src={props.avatar} alt="avatar" className={styles.avatar} />
-      <p className={styles.name}>{props.name}</p>
-      <p className={styles.tag}>{props.tag}</p>
-      <p className={styles.location}>{props.location}</p>
+const ProfileCard = ({ props }) => {
+  let {
+    avatar,
+    name,
+    tag,
+    location,
+    stats: { followers, views, likes }
+  } = props;
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="avatar" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+      <ul className={styles.stats}>
+        <li>
+          <span className={styles.label}>followers</span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
     </div>
-    <ul className={styles.stats}>
-      <li>
-        <span className={styles.label}>followers</span>
-        <span className={styles.quantity}>{props.stats.followers}</span>
-      </li>
-      <li>
-        <span className={styles.label}>Views</span>
-        <span className={styles.quantity}>{props.stats.views}</span>
-      </li>
-      <li>
-        <span className={styles.label}>Likes</span>
-        <span className={styles.quantity}>{props.stats.likes}</span>
-      </li>
-    </ul>
-  </div>
-);
+  );
+};
 
 ProfileCard.propTypes = {
   props: PropTypes.shape({
@@ -36,7 +45,7 @@ ProfileCard.propTypes = {
     stats: PropTypes.shape({
       followers: PropTypes.number.isRequired,
       views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired
     })
   })
 };
